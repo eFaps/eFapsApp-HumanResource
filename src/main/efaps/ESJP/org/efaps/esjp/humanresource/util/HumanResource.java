9b065fc545d365c62f5097a8f74064fda14c51d4
Jan Moxter <jan.moxter@innobix.com>
2017-02-22 19:23:18 -0500

    - Issue #18: Add access to classification via SystemConfiguration
    closes #18

--- src/main/efaps/ESJP/org/efaps/esjp/humanresource/util/HumanResource.java
+++ src/main/efaps/ESJP/org/efaps/esjp/humanresource/util/HumanResource.java
@@ -49,127 +49,167 @@ public final class HumanResource
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATEALL = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateAll")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateAll")
                     .description("Activates T-Registro, related persons, hirachic relations, previous works etc.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATETR = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateTRegistro")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateTRegistro")
                     .description("Activate the classifcation and mechanism realted to T-Registro");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATEREALTED = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateRelated")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateRelated")
                     .description("Activate the possibilty to register related persons. (family etc.)");
 
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATEREALTEDEM = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateRelatedEmergency")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateRelatedEmergency")
                     .description("Activate the possibilty to register related persons fro emergency.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATEHIRACHY = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateHirachy")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateHirachy")
                     .description("Activate the possibilty to register hirachic relations");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATEPREWORK = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivatePreviousWorks")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivatePreviousWorks")
                     .description("Activate the possibilty to register previous works.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATEEDU = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateEducation")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateEducation")
                     .description("Activate the possibilty to register education.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATEREF = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateWorkReferences")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateWorkReferences")
                     .description("Activate the possibilty to register Work References.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATETRAINING = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateTraining")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateTraining")
                     .description("Activate the possibilty to register trainings.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATEDOCS = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateDocuments")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateDocuments")
                     .description("Activate the possibilty to assign documents.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATECONTACTS = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateContacts")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateContacts")
                     .description("Activate the possibilty to assign contacts.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATEACTIVATIONGRP = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateActivationGroup")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateActivationGroup")
                     .description("");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final BooleanSysConfAttribute ACTIVATEIMAGE = new BooleanSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ActivateImages")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ActivateImages")
                     .description(" Activate the image menu.");
 
     /** See description. */
     @EFapsSysConfAttribute
+    public static final BooleanSysConfAttribute CLASS_FIN_ACT = new BooleanSysConfAttribute()
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "classification.FinancialInformation.Activate")
+                    .defaultValue(true)
+                    .description(" Activate the FinancialInformation classification.");
+
+    /** See description. */
+    @EFapsSysConfAttribute
+    public static final BooleanSysConfAttribute CLASS_ADDINFO_ACT = new BooleanSysConfAttribute()
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "classification.AdditionalInfo.Activate")
+                    .defaultValue(true)
+                    .description(" Activate the AdditionalInfo classification.");
+
+    /** See description. */
+    @EFapsSysConfAttribute
+    public static final BooleanSysConfAttribute CLASS_BIRTHPLACE_ACT = new BooleanSysConfAttribute()
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "classification.BirthPlace.Activate")
+                    .defaultValue(true)
+                    .description(" Activate the BirthPlace classification.");
+
+    /** See description. */
+    @EFapsSysConfAttribute
+    public static final BooleanSysConfAttribute CLASS_LOCATION_ACT = new BooleanSysConfAttribute()
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "classification.Location.Activate")
+                    .defaultValue(true)
+                    .description(" Activate the Location classification.");
+
+    /** See description. */
+    @EFapsSysConfAttribute
+    public static final BooleanSysConfAttribute CLASS_TR_ACT = new BooleanSysConfAttribute()
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "classification.TR.Activate")
+                    .defaultValue(true)
+                    .description(" Activate the TR classification.");
+
+    /** See description. */
+    @EFapsSysConfAttribute
     public static final PropertiesSysConfAttribute IMAGEPROPERTIES = new PropertiesSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "ImagesProperties")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "ImagesProperties")
                     .description(" Properties for employee image.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final StringSysConfAttribute DIIDE = new StringSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "DataImport4IDE")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "DataImport4IDE")
                     .description(" Import xml.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final StringSysConfAttribute DI4IDE = new StringSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "DataImport4IDE")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "DataImport4IDE")
                     .description(" Import xml.");
 
     /** See description. */
     @EFapsSysConfAttribute
     public static final StringSysConfAttribute DI4SSA = new StringSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "DataImport4SSA")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "DataImport4SSA")
                     .description(" Import xml.");
     /** See description. */
     @EFapsSysConfAttribute
     public static final StringSysConfAttribute DI4TRA = new StringSysConfAttribute()
-                    .sysConfUUID(SYSCONFUUID)
-                    .key(BASE + "DataImport4TRA")
+                    .sysConfUUID(HumanResource.SYSCONFUUID)
+                    .key(HumanResource.BASE + "DataImport4TRA")
                     .description(" Import xml.");
 
     /**
@@ -187,7 +227,7 @@ public final class HumanResource
         throws CacheReloadException
     {
         // HummanResource-Configuration
-        return SystemConfiguration.get(SYSCONFUUID);
+        return SystemConfiguration.get(HumanResource.SYSCONFUUID);
     }
 
 }
